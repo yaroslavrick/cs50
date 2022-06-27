c1a0f44571781850dd80d4bdbafac14186595e4b
Yaroslav Y <yaroslavrick@gmail.com>
2022-06-27 20:33:47 +0300

    cash in ruby ps01 done + some changes

--- psets/01/cash/ruby/cash.rb
+++ psets/01/cash/ruby/cash.rb
@@ -1,27 +1,26 @@
-require_relative '/lib/cash_methods'
+require_relative 'lib/cash_methods'
 
+# Ask how many cents the customer is owed
+cents = user_input_cents
 
-    # Ask how many cents the customer is owed
-    int cents = get_cents();
+# Calculate the number of quarters to give the customer
+quarters = calculate_quarters(cents)
+cents -= quarters * 25
 
-    # Calculate the number of quarters to give the customer
-    int quarters = calculate_quarters(cents);
-    cents = cents - quarters * 25;
+# Calculate the number of dimes to give the customer
+dimes = calculate_dimes(cents)
+cents -= dimes * 10
 
-    # Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
+# Calculate the number of nickels to give the customer
+nickels = calculate_nickels(cents)
+cents -= nickels * 5
 
-    # Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
+# Calculate the number of pennies to give the customer
+pennies = calculate_pennies(cents)
+cents -= pennies
 
-    # Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
+# Sum coins
+coins = quarters + dimes + nickels + pennies
 
-    # Sum coins
-    int coins = quarters + dimes + nickels + pennies;
-
-    # Print total number of coins to give the customer
-    printf("%i\n", coins);
+# Print total number of coins to give the customer
+puts "#{coins}\n"
